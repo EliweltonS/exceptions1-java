946f816841b54969d8a41d5f7374f3957af17897
eliwelton silva <eliwelton.rc@gmail.com>
2023-01-26 13:16:05 -0300

    IllegalArgumentException

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -34,17 +34,16 @@ public class Reservation {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	public String updatDates(Date checkIn, Date checkOut) {	
+	public void updatDates(Date checkIn, Date checkOut) {	
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for update must be future dates";
+			throw new IllegalArgumentException("Reservation dates for update must be future dates");
 		}
 		if (!checkOut.after(checkIn)) {
-			 return "Check-out date must be after check-in date";
+			 throw new IllegalArgumentException("Check-out date must be after check-in date");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 	
 	@Override
